a0d2698cf498554271c68b4064fea4e44ef357a6
AbdSalehh <abdshaleh124@webmail.umm.ac.id>
2024-05-24 21:06:31 +0700

    fix:fixed skip button

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,7 +8,7 @@ module.exports = merge(common, {
     devServer: {
         static: path.resolve(__dirname, 'dist'),
         open: true,
-        port: 9045,
+        port: 9047,
         client: {
             overlay: {
                 errors: true,
